a51674bdb3e34b9fa40a0342d44cac63c565e027
Chavoosh-Shams <chavoosh84@gmail.com>
2026-02-25 17:40:02 +0330

    Add project files.

---
+++ SinglePageArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SinglePageArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
