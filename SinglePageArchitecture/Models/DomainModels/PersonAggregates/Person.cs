6f14147ee6080fb456a72b6e11549a190a72c81f
Chavoosh-Shams <chavoosh84@gmail.com>
2026-02-25 17:49:11 +0330

    PersonAggregates

---
+++ SinglePageArchitecture/Models/DomainModels/PersonAggregates/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace SinglePageArchitecture.Models.DomainModels.PersonAggregates
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
